8e0a2781f350d25cacb6e40f65018aa2beebebed
Craig Humphreys <craig_h_411@yahoo.co.uk>
2017-11-01 15:02:38 +0000

    Add glyphs folder to package.json

---
+++ examples/SFC/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<fa-rating :glyph="star"></fa-rating>
+	</div>
+</template>
+
+<script type="text/javascript">
+import VueRateIt from '../src/index.js'
+import Star from '../glyphs/star'
+
+export default{
+  components:{
+  	'fa-rating': VueRateIt.FaRating
+  },
+  created(){
+  	this.star = Star;
+  },
+  data(){
+  	return{
+  		star: ''
+  	}
+  }
+}
+
+</script>
